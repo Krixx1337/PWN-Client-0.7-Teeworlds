0364fe26e1acb84f826d5a9e1e3212c2ff067251
Niklas <niklas.flocken@stud.hn.de>
2020-02-09 22:49:21 +0100

    test

---
+++ Testfile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Niklas on 09.02.2020.
+//
+
+#include "Testfile.h"
---
+++ Testfile.h
@@ -0,0 +1,14 @@
+//
+// Created by Niklas on 09.02.2020.
+//
+
+#ifndef TEEWORLDS_0_7_TESTFILE_H
+#define TEEWORLDS_0_7_TESTFILE_H
+
+
+class Testfile {
+
+};
+
+
+#endif //TEEWORLDS_0_7_TESTFILE_H
